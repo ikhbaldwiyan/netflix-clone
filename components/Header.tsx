5475138b169d96ca9d036e77bb71cd8290c3c9c0
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2022-09-05 17:05:31 +0700

    create header component and add react icons

---
+++ components/Header.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import { IoMdSearch } from 'react-icons/io';
+import { AiFillBell } from 'react-icons/ai';
+import Link from "next/link";
+
+function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+
+  }, []);
+
+  return (
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+      <div className="flex items-center space-x-4 md:space-x-10">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />     
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="menu">Home</li>
+          <li className="menu">TV</li>
+          <li className="menu">Movies</li>
+          <li className="menu">New & Popular</li>
+          <li className="menu">My List</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm">
+        <IoMdSearch className="hidden h-6 w-6 sm:inline" />
+        <AiFillBell className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://occ-0-1190-2774.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import Head from "next/head"
+import Header from "../components/Header"
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <p className="text-red-500 text-3xl">
-          Welcome to <a className="font-semibold">Netflix Clone</a>
-        </p>
-      </main>
+    <div className="relative h-screen bg-gradient-to-b from-gray-800/10 to-[#010511] lg:h-[140vh]">
+      <Head>
+        <title>Netflix Clone</title>
+      </Head>
+      <Header />
     </div>
   )
 }
