3824ecb2ad09cb129c2009cc264d7e7aba1b6899
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2022-09-06 13:11:47 +0700

    add api endpoint movies and banner component

---
+++ components/Banner.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image"
+import React, { useEffect, useState } from 'react'
+import { baseUrl } from "../constants/movie";
+import { Movie } from "../types"
+import { FaPlay } from "react-icons/fa";
+import { IoMdInformationCircle } from "react-icons/io";
+
+interface Props {
+  netflixOriginals: Movie[]
+}
+
+function Banner({ netflixOriginals } : Props) {
+  const [movie, setMovie] = useState<Movie | null>();
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length )]
+    )
+  }, [netflixOriginals])
+  
+
+  return (
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+      <div className="absolute -z-10 top-0 left-0 h-[95vh] w-screen">
+        <Image 
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"   
+          objectFit="cover"
+        />
+      </div>
+      <div className="px-4 lg:px-8 space-y-4">
+        <h1 className="text-2xl md:text-4xl lg:text-5xl">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-xl">
+          {movie?.overview}
+        </p>
+        <div className="flex space-x-3 py-4">
+          <button className="banner-btn bg-white text-black">
+            <FaPlay className="h-4 w-4 text-black md:h-6 md:w-6" /> Play
+          </button>
+          <button className="banner-btn bg-gray-400/70">
+          <IoMdInformationCircle className="h-5 w-5 md:h-8 md:w-8" /> More Info 
+          </button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Banner
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,77 @@
-import type { NextPage } from 'next'
 import Head from "next/head"
+import Banner from "../components/Banner"
 import Header from "../components/Header"
+import { Movie } from "../types"
+import api from "../utils/api"
 
-const Home: NextPage = () => {
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home = ({ 
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow
+}: Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-800/10 to-[#010511] lg:h-[140vh]">
       <Head>
-        <title>Netflix Clone</title>
+        <title>Home - Netflix Clone</title>
       </Head>
       <Header />
+      <main>  
+        <Banner netflixOriginals={netflixOriginals}  />
+      </main>
     </div>
   )
 }
 
-export default Home
+export default Home;
+
+export const getServerSideProps = async () => {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(api.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(api.fetchTrending).then((res) => res.json()),
+    fetch(api.fetchTopRated).then((res) => res.json()),
+    fetch(api.fetchActionMovies).then((res) => res.json()),
+    fetch(api.fetchComedyMovies).then((res) => res.json()),
+    fetch(api.fetchHorrorMovies).then((res) => res.json()),
+    fetch(api.fetchRomanceMovies).then((res) => res.json()),
+    fetch(api.fetchDocumentaries).then((res) => res.json()),
+  ]);
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    }
+  }
+
+}
